f58f1788f79fd4b31b5d2fc90d9bb679e37a9fd1
Minh Quan <you@example.com>
2025-07-07 15:41:49 +0700

    update 6

--- routes/reader.js
+++
@@ -1,7 +0,0 @@
-// routes/reader.js
-const express = require("express");
-const router = express.Router();
-const { getAllImagesByMangaId } = require("../controllers/readerController");
-
-router.get("/manga/:mangaid/full", getAllImagesByMangaId);
-module.exports = router;
